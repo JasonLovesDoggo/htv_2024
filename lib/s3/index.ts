f5b2555f78b5f0741017ed0104d7005137be6b36
Jason Cameron <git@jasoncameron.dev>
2024-10-06 09:29:28 -0400

    reference new s3 bucket

--- lib/s3/index.ts
+++ lib/s3/index.ts
@@ -4,7 +4,7 @@ import AWS from "aws-sdk";
 const endpoint = process.env.SERVER_URL?.replace("/first-bucket", "");
 
 export const s3 = new AWS.S3({
-  endpoint: "http://127.0.0.1:9000",
+  endpoint: endpoint,
   accessKeyId: process.env.S3_ACCESS_KEY,
   secretAccessKey: process.env.S3_SECRET_KEY,
   s3ForcePathStyle: true,
